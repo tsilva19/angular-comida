2a1d96d2f7cb20879136e57537da4dd5fb00c71d
Thiago <thiagoalves060@gmail.com>
2022-01-29 17:41:34 -0300

    novo componente

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant.component.css']
 })
 export class RestaurantComponent implements OnInit {
+
+  @Input() restaurant: Restaurant
 
   constructor() { }
 
